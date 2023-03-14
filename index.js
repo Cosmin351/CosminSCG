a30eb8b8571b8fc26a667a221169c504861a3abe
Cosmin351 <74771773+Cosmin351@users.noreply.github.com>
2023-03-14 13:36:46 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function Dataa(){
     document.getElementById("data-ora").innerHTML=dt;
     setTimeout(Dataa,1000)
 }
+function Discord(){
+    const dt ="CosminSCG#3518";
+    navigator.clipboard.writeText(dt);
+}
+
